9da4b30ffaabd4ce1b557f1ecec2921c2b3bd207
liuxing <liuxing@admaster.com.cn>
2020-05-22 12:39:12 +0800

    dep(s-url): build

--- build-s-url.sh
+++ build-s-url.sh
@@ -7,7 +7,7 @@ npm run tsc
 
 sed -i "" "s/1.0.0/${VERSION}/" k8s/package.json
 
-docker build -f k8s/Dockerfile -t hub.mlamp.cn/tuice/wb-msg:${VERSION} .
+docker build -f k8s/Dockerfile -t hub.mlamp.cn/tuice/s-url:${VERSION} .
 
 git checkout k8s/package.json
 
